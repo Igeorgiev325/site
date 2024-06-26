f548865aa952eaba3c4c0d0d2045fa9021ea0ea3
Ivan Georgiev <igeorgiev325@gmail.com>
2024-06-26 21:40:44 +0300

    removed links

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -5,11 +5,7 @@
       <RouterLink to="/about">About</RouterLink>
       <RouterLink to="/district">District</RouterLink>
       <RouterLink to="/counter">Counter</RouterLink>
-      <RouterLink to="/review">Review</RouterLink>
       <RouterLink to="/moveHere">Move Here</RouterLink>
-      <RouterLink to="/parent">Parent</RouterLink>
-      <RouterLink to="/book">Book</RouterLink>
-      <RouterLink to="/promises">Promises</RouterLink>
     </nav>
   </div>
 </template>
